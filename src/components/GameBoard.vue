7caf805910e290dacc1b8a371fa3bb01f3d15485
Alex Matsuev <alex.matsuev@gmail.com>
2023-12-09 15:43:16 +0700

    Update

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -21,16 +21,55 @@ const Snake = reactive([])
 const dCol = ref(0)
 const dRow = ref(-1)
 
+var interval = 0
+
 onBeforeMount(() =>
 {
    InitBoard()
+   Board[2][5] = 3
    InitSnake()
+   window.addEventListener('keydown', onKeyDown)
 })
 
 onMounted(() =>
 {
-   setInterval(() => RenderSnake(), 1000)
+   interval = setInterval(() => RenderSnake(), 250)
 })
+
+function onKeyDown(evt)
+{
+   switch (evt.key)
+   {
+      case 'ArrowUp':
+         if (dRow.value != 1)
+         {
+            dRow.value = -1
+            dCol.value = 0
+         }
+         break
+      case 'ArrowDown':
+         if (dRow.value != -1)
+         {
+            dRow.value = 1
+            dCol.value = 0
+         }
+         break
+      case 'ArrowLeft':
+         if (dCol.value != 1)
+         {
+            dRow.value = 0
+            dCol.value = -1
+         }
+         break
+      case 'ArrowRight':
+         if (dCol.value != -1)
+         {
+            dRow.value = 0
+            dCol.value = 1
+         }
+         break
+   }
+}
 
 function InitBoard()
 {
@@ -68,23 +107,63 @@ function RenderSnake()
    Head.col = Head.col + dCol.value
    Head.row = Head.row + dRow.value
 
-   if (Head.row < 0 || Head.row >= 25 || Head.Col < 0 || Head.Col >= 25)
+   if (Head.row < 0 || Head.row > 25 || Head.Col < 0 || Head.Col > 25)
    {
-
+      GameOver()
    }
 
    Snake[0].seg = 2
 
-   Snake.unshift(Head)
+   // Snake.unshift(Head)
 
    // Хвост
 
-   let Tail = Snake.pop()
+   if (Board[Head.row][Head.col] == 3)
+   {
+      let B = NewTarget()
+      Board[B.row][B.col] = 3
+      Snake.unshift(Head)
+      console.log("Take target")
+   } else
+   {
+      Snake.unshift(Head)
+      let Tail = Snake.pop()
 
-   Snake.forEach((cell) => Board[cell.row][cell.col] = cell.seg)
-   Board[Tail.row][Tail.col] = 0
+      Snake.forEach((cell) => Board[cell.row][cell.col] = cell.seg)
+      Board[Tail.row][Tail.col] = 0
+   }
 }
 
+function NewTarget()
+{
+   let col = Math.floor(Math.random() * 24)
+   let row = Math.floor(Math.random() * 24)
+
+   let a = Snake.filter((item) =>
+   {
+      if ((item.col == col) && (item.row == row))
+      {
+         return true
+      } else
+      {
+         return false
+      }
+   })
+
+   if (a.length != 0)
+   {
+      return NewTarget()
+   } else
+   {
+      return { col: col, row: row }
+   }
+}
+
+function GameOver()
+{
+   clearInterval(interval)
+   console.log("Game Over")
+}
 </script>
 
 <style>
